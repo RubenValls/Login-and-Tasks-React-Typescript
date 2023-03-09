b86ff67c4ad2d631c3e1441de98b11f246f2cc5d
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-09 20:37:00 +0100

    develop branch second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Working on develop
+        Working on develop, second commit.
       </header>
     </div>
   );
