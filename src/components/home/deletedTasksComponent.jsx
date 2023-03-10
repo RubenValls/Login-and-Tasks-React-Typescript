dfd856b111447436f536c3c0efe46fd3fe4363d6
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-10 21:03:20 +0100

    added table structure and info using props

--- src/components/home/deletedTasksComponent.jsx
+++ src/components/home/deletedTasksComponent.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
-const DeletedTasksComponent = () => {
+const DeletedTasksComponent = (props) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(props.data);
+    }, []);
+    
     return (
         <div className = 'flex w-full h-auto justify-content-center align-content-center align-items-center'>
             <div className='w-11 h-auto m-auto'>
-                <div className="grid m-2">
-                    <div className="col bg-blue-100">DELETED TASKS TABLE</div>
+                <div className="card">
+                    <DataTable value={products} sortField="priority" sortOrder={1} tableStyle={{ minWidth: '50rem' }}>
+                        <Column field="task" header="Name" sortable style={{ width: '20%' }}></Column>
+                        <Column field="description" header="Description" sortable style={{ width: '20%' }}></Column>
+                        <Column field="priority" header="Priority" sortable style={{ width: '20%' }}></Column>
+                    </DataTable>
                 </div>
             </div>
         </div>
--- src/components/home/homeComponent.jsx
+++ src/components/home/homeComponent.jsx
@@ -5,20 +5,34 @@ import DeletedTasksComponent from './deletedTasksComponent';
 import PendingTasksComponent from './pendingTasksComponent';
 
 const HomeComponent = () => {
+    
+    const data = [
+        {
+            "task": "taskname",
+            "description": "description",
+            "priority": 1,
+        },
+        {
+            "task": "taskname2",
+            "description": "description2",
+            "priority": 2,
+        }
+    ]
+    
     return (
         <div className = 'w-full h-screen'>
             <TabView>
                 <TabPanel header="Create a new task">
                     CREATE TASK FORM
                 </TabPanel>
-                <TabPanel header="Pending tasks">
-                    <PendingTasksComponent/>
+                <TabPanel header="Tasks">
+                    <PendingTasksComponent data = {data}/>
                 </TabPanel>
                 <TabPanel header="Finished tasks">
-                    <FinishedTasksComponent/>
+                    <FinishedTasksComponent data = {data}/>
                 </TabPanel>
                 <TabPanel header="Deleted tasks">
-                    <DeletedTasksComponent/>
+                    <DeletedTasksComponent data = {data}/>
                 </TabPanel>
             </TabView>
         </div>
