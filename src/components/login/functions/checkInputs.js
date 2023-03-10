9a091d57f3895a7863aec2d3d31b8d986f676bd1
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-10 11:32:02 +0100

    responsive design to mobile

--- src/components/login/functions/checkInputs.js
+++ src/components/login/functions/checkInputs.js
@@ -16,7 +16,7 @@ export function validateForm(email, pass, msg){
     }else{
         if(!errorShow){
             msg.current.show([
-                { severity: 'error', summary: '', detail: 'Test with: eve.holt@reqres.in : cityslicka', sticky: true, closable: false }
+                { severity: 'error', summary: '', detail: 'Test: eve.holt@reqres.in : cityslicka', sticky: true, closable: false }
             ]);
             errorShow = true;
         }
