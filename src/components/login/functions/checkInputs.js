449f23bdb647a0e652f2cbeeb778844b425456cf
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-10 11:15:15 +0100

    login working correctly

--- src/components/login/functions/checkInputs.js
+++ src/components/login/functions/checkInputs.js
@@ -1,34 +1,44 @@
 import axios from "axios";
 
+let errorShow = false;
+
 export function validateForm(email, pass, msg){
     const validateEmailRegex = /^\S+@\S+\.\S+$/
 
     if(validateEmailRegex.test(email)){
         msg.current.clear()
-        msg.current.show([
-            {sticky: true, severity: 'success', summary: '', detail: 'Correct Log in', closable: false}
-        ]);
+
         const loginInfo = {
             "email": email,
             "password": pass
         }
-        axiosRequest(loginInfo)
+        axiosRequest(loginInfo, msg)
     }else{
-        msg.current.show([
-            { severity: 'error', summary: '', detail: 'Test with: eve.holt@reqres.in : cityslicka', sticky: true, closable: false }
-        ]);
+        if(!errorShow){
+            msg.current.show([
+                { severity: 'error', summary: '', detail: 'Test with: eve.holt@reqres.in : cityslicka', sticky: true, closable: false }
+            ]);
+            errorShow = true;
+        }
     }
 }
 
-function axiosRequest(loginInfo){
+function axiosRequest(loginInfo, msg){
 
     const apiURL = 'https://reqres.in/api/login'
 
     axios.post(apiURL, loginInfo)
     .then(function (response) {
-        console.log(response.data.token);
+        if(response.status === 200){
+            msg.current.show([
+                {sticky: true, severity: 'success', summary: '', detail: 'Correct Log in', closable: false}
+                ]);
+        }
+        console.log(response);
     })
     .catch(function (error) {
-        console.log(error);
+        msg.current.show([
+            { severity: 'error', summary: '', detail: 'Test with: eve.holt@reqres.in : cityslicka', sticky: true, closable: false }
+        ]);
     });
 }
